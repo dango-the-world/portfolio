ab3c6670186b573fe547e3e0f3dfd16d051dbced
dango-the-world <z.rohaso@gmail.com>
2024-08-05 22:32:12 +0900

    add skills component

---
+++ src/app/components/Skills.tsx
@@ -0,0 +1,28 @@
+import { Box, Image } from "@yamada-ui/react";
+import React from "react";
+import { skills } from "../utils/skilData";
+import { skillInterface } from "../types/skillInterface";
+
+export const Skills = () => {
+  return (
+    <>
+      <Box as="center" pb={"10px"}></Box>
+      <Box
+        display={"flex"}
+        flexWrap={"wrap"}
+        justifyContent={"center"}
+        w={"100%"}
+        gap={"10px"}
+      >
+        {skills.map((skill: skillInterface) => (
+          <Image
+            key={skill.id}
+            src={skill.skill_logo}
+            alt={skill.skill_name}
+            h={"50px"}
+          />
+        ))}
+      </Box>
+    </>
+  );
+};
